0e330c525b0d157e2dceb66fd4059e0e5b02b9fa
wangjwchn <wangjwchn@yahoo.com>
2016-04-01 16:36:15 +0800

    Add AccFalseColorFilter

---
+++ MetalAcc/AccFalseColorFilter.swift
@@ -0,0 +1,20 @@
+//
+//  AccFalseColorFilter.swift
+//  MetalAcc
+//
+//  Created by 王佳玮 on 16/4/1.
+//  Copyright © 2016年 JW. All rights reserved.
+//
+
+import Foundation
+class AccFalseColorFilter: AccImageFilter {
+    var firstColor:(R:Float,G:Float,B:Float)?
+    var secondColor:(R:Float,G:Float,B:Float)?
+    override init(){
+        super.init()
+        self.name = "FalseColor"
+    }
+    override func applyFilter() {
+        addCommandWithFactor([firstColor!.R,firstColor!.G,firstColor!.B,secondColor!.R,secondColor!.G,secondColor!.B])
+    }
+}
